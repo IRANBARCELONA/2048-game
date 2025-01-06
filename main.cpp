84287ac22a619624621e1fb4621a841cb0155daf
unknown <nimahassani84@gmail.com>
2025-01-06 12:46:14 +0330

    single 2048 project README

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+// Do we need this ?
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void printboard() { return; }
+void randomnumber() { return; }
+void checkWinOrLose() { return; }
+void Movement() { return; }
+// options :
+// move and change board locarion
+// make function for left right up and top
+
+void Merge(vector<int> line) { return; }
+// input can be anything
+
+
+
+int main(void)
+{
+    return 0;
+}
