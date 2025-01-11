ddd67641eb3e03a3d53197d804a2575caf990cb4
IRANBARCELONA <www.reza5567880@gmail.com>
2025-01-11 17:00:20 +0330

    Graphical changes

--- main.cpp
+++ main.cpp
@@ -1,74 +1,32 @@
 #include <iostream>
-#include <string>
-#include <cstdlib>
-#include <ctime>
+#include <limits>
 using namespace std;
+int main() {
 
-int SIZE = 4;
-char board[4][4] = {{' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' '}};
-void RandomGenerator()
-{
-
-    while (true)
-    {
-        srand(static_cast<unsigned>(time(0)));
-        int randomRow = rand() % 4;
-        int randomCol = rand() % 4;
-        if (board[randomRow][randomCol] == ' ')
-        {
-            board[randomRow][randomCol] = '2';
-            break;
+    int a,b[2],c,d;
+    int max=numeric_limits<int>::min();
+    int min=numeric_limits<int>::max();
+    for (int i=0;i<2;i++){
+        cin>>b[i];
+        if(max<b[i])
+            max=b[i];
+        if(min>b[i])
+            min=b[i];
+        if(b[i]>min and b[i]<max)
+            a=b[i];
+    }
+    if(max!=min){
+        min=max;
+        d++;
+        if(a!=max){
+            c=b;
+            d++;
         }
     }
-}
-void printBoard()
-{
-
-    string h_line = "_____|_____|_____|_____";
-    string v_line = "     |     |     |     ";
-    string star_line = "***************************";
-    string indent = "\t\t\t"; // 4 indent to reach board = 48 space
-    cout << indent << indent << indent << star_line << endl;
-    cout << indent << indent << indent << "**  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << "  |  " << board[0][3] << "  **" << endl;
-    cout << indent << indent << indent << "**" << h_line << "**" << endl;
-    cout << indent << indent << indent << "**  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << "  |  " << board[1][3] << "  **" << endl;
-    cout << indent << indent << indent << "**" << h_line << "**" << endl;
-    cout << indent << indent << indent << "**  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << "  |  " << board[2][3] << "  **" << endl;
-    cout << indent << indent << indent << "**" << h_line << "**" << endl;
-    cout << indent << indent << indent << "**  " << board[3][0] << "  |  " << board[3][1] << "  |  " << board[3][2] << "  |  " << board[3][3] << "  **" << endl;
-    cout << indent << indent << indent << "**" << v_line << "**" << endl;
-    cout << indent << indent << indent << star_line << endl;
-}
-
-int main()
-{
-    int row, col;
-    RandomGenerator();
-    printBoard();
-
-     char PlayerMovement = ' ';
-    cin >> PlayerMovement;
-
-    while (true)
-    {
-        switch (PlayerMovement)
-        {
-        case 'd':
-            MoveRight(board);
-            break;
-        case 's':
-            MoveDown(board);
-        case 'a':
-            MoveLeft(board);
-        case 'w':
-            MoveUp(board);
-        default:
-            cout << "INVALID INPUT !" << endl;
-            break;
-        }
+    else if(b!=c){
+        b=c;
+        d++;
     }
-
-   
-    // system("pause");
+    cout<<d;
     return 0;
 }
