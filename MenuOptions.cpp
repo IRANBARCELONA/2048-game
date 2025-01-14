e9433251d425677d338e5efec86001ac15506db1
nima-hassani <nimahassani84@gmail.com>
2025-01-14 21:28:23 +0330

    Save and Load option added

--- MenuOptions.cpp
+++ MenuOptions.cpp
@@ -3,11 +3,13 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
-#include <thread>
+#include <unistd.h>
+#include <fstream>
 using namespace std;
 
-const int SIZE = 4;
-vector<vector<int>> board = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+#define SIZE 4
+
+// Game operators
 bool checklose(vector<vector<int>> &board)
 {
     for (int i = 0; i < SIZE; i++)
@@ -24,8 +26,7 @@ bool checklose(vector<vector<int>> &board)
     }
     return true;
 }
-
-void RandomGenerator()
+void RandomGenerator(vector<vector<int>> &board)
 {
 
     while (true)
@@ -41,25 +42,93 @@ void RandomGenerator()
     }
 }
 
-void printBoard()
+// Board drawing
+void snow()
 {
-
+    int r = rand() % 23;
+    for (int i = 0; i < 23; i++)
+    {
+        if (i == r)
+            cout << "*";
+        cout << " ";
+    }
+}
+void space(int a)
+{
+    int n = 0, ox = a;
+    while (a > 0)
+    {
+        n++;
+        a /= 10;
+    }
+    if (ox != 0)
+    {
+        for (int i = 0; i < 4 - n; i++)
+            cout << " ";
+    }
+    else
+        cout << "   ";
+}
+void printBoard(vector<vector<int>> &board)
+{
     string h_line = "_____|_____|_____|_____";
     string v_line = "     |     |     |     ";
     string star_line = "***************************";
-    string indent = "\t\t\t"; // 4 indent to reach board = 48 space
-    cout << indent << indent << indent << star_line << endl;
-    cout << indent << indent << indent << "**  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << "  |  " << board[0][3] << "  **" << endl;
-    cout << indent << indent << indent << "**" << h_line << "**" << endl;
-    cout << indent << indent << indent << "**  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << "  |  " << board[1][3] << "  **" << endl;
-    cout << indent << indent << indent << "**" << h_line << "**" << endl;
-    cout << indent << indent << indent << "**  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << "  |  " << board[2][3] << "  **" << endl;
-    cout << indent << indent << indent << "**" << h_line << "**" << endl;
-    cout << indent << indent << indent << "**  " << board[3][0] << "  |  " << board[3][1] << "  |  " << board[3][2] << "  |  " << board[3][3] << "  **" << endl;
-    cout << indent << indent << indent << "**" << v_line << "**" << endl;
-    cout << indent << indent << indent << star_line << endl;
+    string indent = "\t\t\t";                                                          // 4 indent to reach board = 48 space
+    cout << indent << "  &&                          " << indent << star_line << endl; //+
+    cout << " /\\   /\\    \\  /  " << "        &&&&      mmm               " << indent << "** " << board[0][0];
+    space(board[0][0]);
+    cout << "| ";
+    cout << board[0][1];
+    space(board[0][1]);
+    cout << "| " << board[0][2];
+    space(board[0][2]);
+    cout << "| " << board[0][3];
+    space(board[0][3]);
+    cout << "**" << endl;
+    cout << " \\   /__\\    \\/      " << "     &&  &         mmm          m" << indent << "**" << h_line << "**" << endl;
+    cout << "  \\ /    \\   /          " << "   &  &      | ---- \\     /--" << indent << "** " << board[1][0];
+    space(board[1][0]);
+    cout << "| " << board[1][1];
+    space(board[1][1]);
+    cout << "| " << board[1][2];
+    space(board[1][2]);
+    cout << "| " << board[1][3];
+    space(board[1][1]);
+    cout << "**" << endl;
+    cout << " \\//      \\ /           " << "   && &      |  &&   |&& |  &&" << indent << "**" << h_line << "**" << endl;
+    cout << indent << "    & &      \\      /     \\    " << indent << "** " << board[2][0];
+    space(board[2][0]);
+    cout << "| " << board[2][1];
+    space(board[2][1]);
+    cout << "| " << board[2][2];
+    space(board[2][2]);
+    cout << "| " << board[2][3];
+    space(board[2][3]);
+    cout << "**" << endl;
+    cout << " \\      /   \\   /       " << "     &&         ----         --" << indent << "**" << h_line << "**" << endl;
+    cout << " |\\    /|    \\ /        " << "      &            /       \\   " << indent << "** " << board[3][0];
+    space(board[3][0]);
+    cout << "| " << board[3][1];
+    space(board[3][1]);
+    cout << "| " << board[3][2];
+    space(board[3][2]);
+    cout << "| " << board[3][3];
+    space(board[3][3]);
+    cout << "**" << endl;
+    cout << " | \\  / |     /         " << "       \\            \\     / " << indent << "**" << v_line << "**" << endl;
+    cout << " |  \\/  |    /          " << "        \\        ///////\\\\\\\\\\" << "\t\t       " << "*                           *" << endl;
+    cout << indent << "       _/\\      ////////\\\\\\\\\\" << "\t\t      " << "*                             *" << endl;
+    cout << " \\   |    /\\    \\     / -----" << " /   \\     ||  _______" << "\t\t     " << "*    ";
+    snow();
+    cout << "   *" << endl;
+    cout << " |\\  |   /__\\   |\\   /| |__ " << "_/     |    ||         " << "\t\t     " << "*                               *" << endl;
+    cout << " | \\ |  /    \\  | \\ / | |  " << "/       \\     \\\\\\\\\\\\\\/////" << "\t\t     " << "*                               *" << endl;
+    cout << " |  \\| /      \\ |  /  | |----" << indent << "\t\t     ******" << star_line;
+    cout << "\n\n";
 }
 
+// Movements
 void MoveLeft(vector<vector<int>> &board)
 {
     for (int k = 0; k < SIZE; k++)
@@ -94,7 +163,6 @@ void MoveLeft(vector<vector<int>> &board)
         }
     }
 }
-
 void MoveRight(vector<vector<int>> &board)
 {
     for (int k = 0; k < SIZE; k++)
@@ -129,7 +197,6 @@ void MoveRight(vector<vector<int>> &board)
         }
     }
 }
-
 void MoveUp(vector<vector<int>> &board)
 {
     for (int j = 0; j < SIZE; j++)
@@ -164,7 +231,6 @@ void MoveUp(vector<vector<int>> &board)
         }
     }
 }
-
 void MoveDown(vector<vector<int>> &board)
 {
     for (int j = 0; j < SIZE; j++)
@@ -199,73 +265,152 @@ void MoveDown(vector<vector<int>> &board)
         }
     }
 }
-void invalidInput(bool &Gogame)
+
+// Saving methods
+void SaveBoard(vector<vector<int>> &board, string &filename)
 {
-    cout << "INVALID INPUT !" << endl;
-    Gogame = false;
-    this_thread::sleep_for(std::chrono::seconds(2));
+    ofstream load(filename);
+    if (load.is_open())
+    {
+        for (auto &row : board)
+        {
+            for (int &number : row)
+            {
+                load << number << " ";
+            }
+            load << endl;
+        }
+
+        load.close();
+    }
+    else
+    {
+        cerr << "The game can not be saved ! " << endl;
+    }
 }
-// void QuitGame(bool &Gogame)
-// {
-//     Gogame = false;
-//     string sure;
-//     cout << "Are sure to exit? (yes/no)";
-//     cin >> sure;
-//     if (sure == "yes")
-//     {
-        
-//     }
-//     else if (sure == "no")
-//     {
-//         /* code */
-//     }
-    
-    
-// }
+vector<vector<int>> LoadSave(string &filename)
+{
+    vector<vector<int>> board(SIZE, vector<int>(SIZE));
+    ifstream load(filename);
+    if (load.is_open())
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                load >> board[i][j];
+            }
+        }
+        load.close();
+        return board;
+    }
+    else
+    {
+        cerr << "Save not found ! " << endl;
+    }
+}
+
 int main()
 {
-    
-    bool goGame = true;
+
+    vector<vector<int>> board;
+    bool RandomValidation = true;
+
+    string filename = "board.txt";
+    ifstream load(filename);
+    if (load.good())
+    {
+        cout << "Loading savedgame..." << endl;
+        board = LoadSave(filename);
+        RandomValidation = false;
+        usleep(3000000);
+    }
+    else
+    {
+        board = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+        char check;
+        cout << "No saved game." << endl;
+        cout << "New Game? (y/n) :";
+        cin >> check;
+        bool x = true;
+        while (x)
+        {
+            if (check == 'y')
+                x = false;
+            else if (check == 'n')
+            {
+                cout << "Bye my friend ...." << endl;
+                usleep(3000000);
+                return EXIT_SUCCESS;
+            }
+            else
+                cout << "what ? (y/n):";
+        }
+    }
+    bool wrongInput = false;
     while (true)
     {
-        if (goGame == true)
-        {
-            RandomGenerator();
-        }
         system("cls");
-        printBoard();
-        bool chaker = checklose(board);
-        if (chaker)
+        if (RandomValidation)
         {
-            cout << "fukin lost";
+            RandomGenerator(board);
+            printBoard(board);
+            cout << "\nDo a move (w/s/a/d) : ";
+        }
+        else
+        {
+            printBoard(board);
+            cout << "save and quit by pressing 'q' " <<endl;
+            if (wrongInput)
+                cout << "Invalid input !  (w/s/a/d) : ";
+            else
+                cout << "\nDo a move (w/s/a/d) : ";
+            
+        }
+
+        bool cheker = checklose(board);
+        if (cheker)
+        {
+            cout << "You Lost ! ";
+
             return 0;
         }
-
         else
-            goGame = true;
+            RandomValidation = true;
 
+        // get input
         char PlayerMovement = ' ';
         cin >> PlayerMovement;
-        switch (PlayerMovement)
+
+        if (PlayerMovement == 'w')
         {
-        case 'd':
-            MoveRight(board);
-            break;
-        case 's':
-            MoveDown(board);
-            break;
-        case 'a':
-            MoveLeft(board);
-            break;
-        case 'w':
             MoveUp(board);
-            break;
-        case 'q':
+            RandomValidation = true;
+        }
+        else if (PlayerMovement == 's')
+        {
+            MoveDown(board);
+            RandomValidation = true;
+        }
+        else if (PlayerMovement == 'a')
+        {
+            MoveLeft(board);
+            RandomValidation = true;
+        }
+        else if (PlayerMovement == 'd')
+        {
+            MoveRight(board);
+            RandomValidation = true;
+        }
+        else if (PlayerMovement == 'q')
+        {
+            SaveBoard(board, filename);
             return 0;
-            break;
-        default:
-            invalidInput(goGame);
-            break;
+        }
+        else
+        {
+            RandomValidation = false;
+            wrongInput = true;
         }
     }
 
